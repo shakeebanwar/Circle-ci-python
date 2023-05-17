40920355b63ad75da10f4d8c8456a6bc60fd0325
Your Name <you@example.com>
2023-05-17 15:59:03 +0500

    circleci build and test

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
         name = "shakeeb anwar"
         upper_name = to_upper(name)
-        self.assertEqual(upper_name, "shakeeb anwar")
+        self.assertEqual(upper_name, "SHAKEEB ANWAR")
 
 
 if __name__ == '__main__':
